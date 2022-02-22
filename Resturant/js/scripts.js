d99e0b7e16ce3ccd31787e6af572657ace92e86d
Andrew Kester Account <10919987@my.uvu.edu>
2022-02-22 13:16:38 -0700

    all finished with the resturant

--- Resturant/js/scripts.js
+++ Resturant/js/scripts.js
@@ -1,8 +1,8 @@
 function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerMenu").classList.toggle("open");
 }
 
 
 const x = document.getElementById("hamburgerMenu");
-
 x.onclick = toggleMenu;
